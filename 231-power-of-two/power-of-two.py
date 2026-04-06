297f4c2011192c047c87f4bbf7bdcab41fa16869
Dilip Bhanushali <137048001+Dilip9090@users.noreply.github.com>
2026-04-06 19:45:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (68.84%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n==0:
+            return False
+
+        while n%2 ==0:
+            n/=2
+
+        return n==1
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
