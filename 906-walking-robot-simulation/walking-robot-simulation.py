504b9f15ad691d27cef379b3760fb63cfa6d306c
Dilip Bhanushali <137048001+Dilip9090@users.noreply.github.com>
2026-04-06 21:55:43 +0530

    Time: 48 ms (68.42%) | Memory: 17.2 MB (47.37%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def robotSim(self, commands, obstacles):
+        # Directions: North, East, South, West
+        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+        d = 0  # start facing north
+        
+        x = y = 0
+        obstacle_set = set(map(tuple, obstacles))
+        
+        max_dist = 0
+        
+        for cmd in commands:
+            if cmd == -1:  # turn right
+                d = (d + 1) % 4
+            elif cmd == -2:  # turn left
+                d = (d - 1) % 4
+            else:
+                dx, dy = dirs[d]
+                for _ in range(cmd):
+                    if (x + dx, y + dy) in obstacle_set:
+                        break
+                    x += dx
+                    y += dy
+                    max_dist = max(max_dist, x*x + y*y)
+        
+        return max_dist
+        """
+        :type commands: List[int]
+        :type obstacles: List[List[int]]
+        :rtype: int
+        """
+        
