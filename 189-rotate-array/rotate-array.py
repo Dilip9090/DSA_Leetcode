90e711a886c0631eddffd18a827779ea690a3937
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-07-02 17:14:07 +0530

    Time: 10 ms (27.44%) | Memory: 23.8 MB (15.37%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,12 +1,33 @@
 class Solution:
     def rotate(self, arr, k):
-
         n = len(arr)
-
         k = k % n
+        temp = []
+        l = 0
+        p = 0
 
-        arr.reverse()
+        for i in range(0,n-k):
+            temp.append(arr[i])
 
-        arr[:k] = reversed(arr[:k])
+        for s in range(n-k,n):
+            arr[l] = arr[s]
+            l += 1
 
-        arr[k:] = reversed(arr[k:])
+        for t in range(k,n):
+            arr[t] = temp[p]
+            p += 1
+        return arr            
+            
+
+
+
+
+        # n = len(arr)
+
+        # k = k % n
+
+        # arr.reverse()
+
+        # arr[:k] = reversed(arr[:k])
+
+        # arr[k:] = reversed(arr[k:])
