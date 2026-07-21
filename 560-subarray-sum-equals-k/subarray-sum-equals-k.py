eedc32b720713bf3f86b1f2d9c333267dbc0b742
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-07-21 18:21:21 +0530

    Time: 41 ms (22.87%) | Memory: 22 MB (34.74%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,35 +1,26 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-
         presum = [0] * n
         presum[0] = nums[0]
+        
 
         for i in range(1, n):
             presum[i] = presum[i - 1] + nums[i]
-
         mpp = {0: 1}
         count = 0
-
         for j in range(n):
             val = presum[j] - k
-
             if val in mpp:
                 count += mpp[val]
-
             if presum[j] in mpp:
                 mpp[presum[j]] += 1
             else:
                 mpp[presum[j]] = 1
-
         return count
-                
 
-        
-        
         # n = len(nums)
         # count = 0
-
 
         # for i in range (n):
         #     sum1 = 0
@@ -37,4 +28,4 @@ class Solution:
         #         sum1 += nums[j]
         #         if sum1 == k:
         #             count += 1
-        # return count            
+        # return count
