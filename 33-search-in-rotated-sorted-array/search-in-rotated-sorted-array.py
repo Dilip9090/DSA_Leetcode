2373ed12466d1c3f87a1810f629255d7541ae678
Dilip Bhanushali <dilipbhanushali321@gmail.com>
2026-09-23 20:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (10.56%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,27 +5,34 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        low = 0
-        high = len(nums) - 1
+        n = len(nums)
 
-        while low <= high:
-
-            mid = (low + high) // 2
-
-            if nums[mid] == target:
-                return mid
- 
-            if nums[low] <= nums[mid]:
-
-                if nums[low] <= target < nums[mid]:
-                    high = mid - 1
-                else:
-                    low = mid + 1
-            else:
-
-                if nums[mid] < target <= nums[high]:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-
+        for i in range(n):
+            if nums[i] == target:
+                return i
         return -1
+        
+        # low = 0
+        # high = len(nums) - 1
+
+        # while low <= high:
+
+        #     mid = (low + high) // 2
+
+        #     if nums[mid] == target:
+        #         return mid
+ 
+        #     if nums[low] <= nums[mid]:
+
+        #         if nums[low] <= target < nums[mid]:
+        #             high = mid - 1
+        #         else:
+        #             low = mid + 1
+        #     else:
+
+        #         if nums[mid] < target <= nums[high]:
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+
+        # return -1
